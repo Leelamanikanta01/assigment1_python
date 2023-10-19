28c84e441bb12cbdccf218318a872ad1b5a22b5e
leelamanikanta01 <leelamanikanta01@gmail.com>
2023-10-19 13:18:27 +0000

    solution

--- Assignment.py
+++ Assignment.py
@@ -1,6 +1,22 @@
-import random
+def roman_to_integer(roman):
+    roman_numerals = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
 
-array = [1, 2, 3, 4, 5, 6, 7]
-random.shuffle(array)
+    result = 0
+    prev_value = 0
 
-print(array)
+    for numeral in reversed(roman):
+        value = roman_numerals[numeral]
+        if value < prev_value:
+            result -= value
+        else:
+            result += value
+        prev_value = value
+
+    return result
+
+
+roman_numeral = "IX"
+integer_value = roman_to_integer(roman_numeral)
+print(integer_value)
